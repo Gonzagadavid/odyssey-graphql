a83b52e8cfc552ccbc4cc180f2bf851d894b460d
gonzagadavid <david.blesses@hotmail.com>
2022-06-03 20:54:36 -0300

    feat: create query to data]

--- graphQL-odyssey/odyssey-lift-off-part1/client/src/pages/tracks.js
+++ graphQL-odyssey/odyssey-lift-off-part1/client/src/pages/tracks.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Layout } from '../components';
-
+import {gql} from '@apollo/client';
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+ const TRACKS = gql`
+ query ExampleQuery {
+  tracksForHome {
+    id
+    title
+    thumbnail
+    length
+    modulesCount
+    author {
+      id
+      name
+      photo
+    }
+  }
+}
+`;
+
 const Tracks = () => {
   return <Layout grid> </Layout>;
 };
